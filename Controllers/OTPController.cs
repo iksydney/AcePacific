4280a02728f357a9611a01a181dfcd4b38e153d4
Ogbonna Ikenna <ikenna.ogbonna@bankly.ng>
2023-09-09 17:37:47 +0100

    swagger implementation:

---
+++ Controllers/OTPController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcePacific.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OTPController : ControllerBase
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using AcePacific.API.ExtensionServices;
+
 namespace AcePacific
 {
     public class Program
@@ -5,17 +7,34 @@ namespace AcePacific
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var services = builder.Services;
+            var config = builder.Configuration;
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.SwaggerExtension();
+
+
+
+
+
+
+
 
             var app = builder.Build();
 
+            if(app.Environment.IsDevelopment())
+            {
+                app.SwaggerDocumentation();
+            }
             // Configure the HTTP request pipeline.
 
             app.UseHttpsRedirection();
-
+            app.UseSwagger();
+            app.UseAuthorization();
             app.UseAuthorization();
 
 
