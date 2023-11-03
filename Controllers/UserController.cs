58f51e3dc5248c0b27ad0b0dbfced125c564cb16
ikenna.ogbonna <ikenna.ogbonna@bankly.ng>
2023-11-03 15:48:27 +0100

    migrations

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AcePacific.Busines.Services;
+using AcePacific.Common.Contract;
+using AcePacific.Data.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AcePacific.API.Controllers
@@ -7,5 +10,24 @@ namespace AcePacific.API.Controllers
     [ApiController]
     public class UserController : BaseApiController
     {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpGet("GetUserByPhoneNumber/{phoneNumber}")]
+        public async Task<ActionResult<Response<PhoneNumberExistsDto>>> CheckifPhoneNumberExists(string phoneNumber)
+        {
+            try
+            {
+                var response = await _userService.CheckPhoneNumberExists(phoneNumber);
+                return Ok(response);
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
     }
 }
